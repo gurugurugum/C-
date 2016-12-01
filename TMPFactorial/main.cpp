b174c99780a8e1048cbf99b710c73647c5db797b
岡 和磨 <kazuma@oka-kazuma-no-macbook-pro.local>
2016-12-01 22:55:13 +0900

    succeeded in computing factorial

--- TMPFactorial/main.cpp
+++ TMPFactorial/main.cpp
@@ -8,8 +8,21 @@
 
 #include <iostream>
 
+template <int N>
+struct Factorial
+{
+	enum { value = N * Factorial<N - 1>::value };
+};
+
+template <>
+struct Factorial<0>
+{
+	enum { value = 1 };
+};
+
 int main(int argc, const char * argv[]) {
 	// insert code here...
 	std::cout << "Hello, World!\n";
+	std::cout << Factorial<6>::value << std::endl;
     return 0;
 }
